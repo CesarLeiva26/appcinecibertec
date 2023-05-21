c1bb9ebf03cfb3cb52456e87952888e9f52ef35f
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-05-21 15:33:53 -0500

    initial commit

--- bin/src/main/resources/static/js/web/home.js
+++
@@ -1,7 +0,0 @@
-$(document).on("click", "#btnempezar", 
-		function(){
-	let usuario = $("#txtusuario").val();
-	let password = $("#txtpassword").val();
-	$("#lblmensaje").text("Usuario : "+usuario +" - Contraseña : " +password);
-	$("#modalempezar").modal("show");
-})
